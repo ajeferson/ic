d5b276946aedbbb4697663568fdbecda77403d18
Alan Jeferson <alan.jeferson11@gmail.com>
2017-09-30 23:51:06 -0300

    Refactor: change underscore case for camel case

--- tic_tac_toe/tic_tac_toe.js
+++ tic_tac_toe/tic_tac_toe.js
@@ -12,32 +12,31 @@ function TicTacToe() {
     this.$turn = null;
     this.turn = true; // true if users turn
     this.board = []; // 1 - user; 0 - computer;
-    this.game_over = false;
+    this.gameOver = false;
 
     this.init = function() {
-        this.cache_dom();
-        this.bind_events();
-        this.board = this.new_board();
+        this.cacheDom();
+        this.bindEvents();
+        this.board = this.newBoard();
         // this.board[0][0] = 0;
         // this.board[0][1] = 0;
         // this.board[0][2] = 1;
         // this.board[1][1] = 1;
         // this.board[2][0] = 0;
         // this.board[2][1] = 1;
-        this.draw_board(this.board);
+        this.drawBoard(this.board);
     };
 
-    this.cache_dom = function() {
+    this.cacheDom = function() {
         this.$squares = $('.square');
         this.$turn = $('.turn');
     };
 
-    this.bind_events = function() {
-        // this.did_click_square.bind(this);
-        this.$squares.on('click', this.did_click_square);
+    this.bindEvents = function() {
+        this.$squares.on('click', this.didClickSquare);
     };
 
-    this.new_board = function() {
+    this.newBoard = function() {
         var matrix = [];
         for(var i = 0; i < 3; i++) {
             var row = [];
@@ -50,20 +49,20 @@ function TicTacToe() {
     };
 
     // Draws only when the next move if from the user
-    this.draw_board = function(board) {
+    this.drawBoard = function(board) {
         for(var i = 0; i < 3; i++) {
             for(var j = 0; j < 3; j++) {
                 if(board[i][j] == 1) {
-                    this.user_check($(this.$squares[3*i + j]));
+                    this.userCheck($(this.$squares[3*i + j]));
                 } else if(board[i][j] == 0) {
-                    this.computer_check($(this.$squares[3*i + j]));
+                    this.computerCheck($(this.$squares[3*i + j]));
                 }
             }
         }
     };
 
-    this.clone_board = function(board) {
-        var cl = this.new_board();
+    this.cloneBoard = function(board) {
+        var cl = this.newBoard();
         for(var i = 0; i < board.length; i++) {
             for(var j = 0; j < board[0].length; j++) {
                 cl[i][j] = board[i][j];
@@ -72,25 +71,25 @@ function TicTacToe() {
         return cl;
     };
 
-    this.print_board = function(b) {
+    this.printBoard = function(b) {
         var str = "";
         for(var i = 0; i < 3; i++) {
-            str += this.printable_char(b[i][0]) + " " + this.printable_char(b[i][1]) + " " + this.printable_char(b[i][2]) + "\n";
+            str += this.printableChar(b[i][0]) + " " + this.printableChar(b[i][1]) + " " + this.printableChar(b[i][2]) + "\n";
         }
         console.log(str);
     };
 
-    this.printable_char = function(c) {
+    this.printableChar = function(c) {
         return c == null ? '-' : c;
     };
 
-    this.get_successors = function(board, t) {
+    this.getSuccessors = function(board, t) {
         var pl = this.player(t);
         var successors = [];
         for(var i = 0; i < board.length; i++) {
             for(var j = 0; j < board[0].length; j++) {
                 if(board[i][j] == null) {
-                    var s = this.clone_board(board);
+                    var s = this.cloneBoard(board);
                     s[i][j] = pl;
                     successors.push([s, i, j]);
                 }
@@ -99,32 +98,32 @@ function TicTacToe() {
         return successors;
     };
 
-    this.did_click_square = function(e) {
-        if(this.game_over || !this.turn) { return; }
+    this.didClickSquare = function(e) {
+        if(this.gameOver || !this.turn) { return; }
         var $target = $(e.target);
         var row = $target.data('row');
         var column = $target.data('column');
         var $square = $target;
         this.board[row][column] = this.player(this.turn);
-        this.user_check($square);
-        this.switch_turn_span();
-        this.switch_turn();
-        this.check_game_over();
-        this.computer_turn();
+        this.userCheck($square);
+        this.switchTurnSpan();
+        this.switchTurn();
+        this.checkGameOver();
+        this.computerTurn();
     }.bind(this);
 
-    this.computer_turn = function() {
-        if(this.game_over || this.turn) { return; }
+    this.computerTurn = function() {
+        if(this.gameOver || this.turn) { return; }
         var node = this.minimax(this.board);
         this.board[node.i][node.j] = this.player(this.turn);
         var index = node.i*3 + node.j;
-        this.computer_check($(this.$squares[index]));
-        this.switch_turn_span();
-        this.check_game_over();
-        this.switch_turn();
+        this.computerCheck($(this.$squares[index]));
+        this.switchTurnSpan();
+        this.checkGameOver();
+        this.switchTurn();
     };
 
-    this.score_state = function(state) {
+    this.scoreState = function(state) {
         for(var i = 0; i < 3; i++) {
             // Horizontal
             if(state[i][0] != null && state[i][0] == state[i][1] && state[i][1] == state[i][2]) {
@@ -149,10 +148,10 @@ function TicTacToe() {
     this.minimax = function(state, t) {
 
         var depth = 2;
-        var root = new TreeNode(this.clone_board(state));
+        var root = new TreeNode(this.cloneBoard(state));
 
         var v = 2; // Min
-        this.x_value(root, t, depth, v, Math.min, Math.max);
+        this.xValue(root, t, depth, v, Math.min, Math.max);
 
         for(var i = 0; i < root.children.length; i++) {
             if(root.children[i].score == root.score) {
@@ -165,16 +164,16 @@ function TicTacToe() {
 
     };
 
-    this.x_value = function(node, t, depth, v, f, o) {
+    this.xValue = function(node, t, depth, v, f, o) {
 
-        var score = this.score_state(node.state);
+        var score = this.scoreState(node.state);
         if(depth == 0 || score != 0) {
             node.score = score;
             return score;
         }
 
         // TODO Needs improvement
-        var successors = this.get_successors(node.state, t);
+        var successors = this.getSuccessors(node.state, t);
         for(var i = 0; i < successors.length; i++) {
             var tn = new TreeNode(successors[i][0]);
             tn.i = successors[i][1];
@@ -185,27 +184,27 @@ function TicTacToe() {
         node.score = v;
         for(var i = 0; i < successors.length; i++) {
             var curr = node.children[i];
-            node.score = f(node.score, this.x_value(curr, !t, depth-1, -v, o, f));
+            node.score = f(node.score, this.xValue(curr, !t, depth-1, -v, o, f));
         }
 
         return node.score
 
     };
 
-    this.user_check = function(elem) {
-        this.mark_with(elem, 'x');
+    this.userCheck = function(elem) {
+        this.markWith(elem, 'x');
     };
 
-    this.computer_check = function(elem) {
-        this.mark_with(elem, 'o');
+    this.computerCheck = function(elem) {
+        this.markWith(elem, 'o');
     };
 
-    this.mark_with = function(elem, symbol) {
+    this.markWith = function(elem, symbol) {
         $(elem).css('background-image', "url('" + symbol + ".png')");
     };
 
     // true if users
-    this.switch_turn_span = function() {
+    this.switchTurnSpan = function() {
         if(this.turn) {
             this.$turn.text("Computer's turn");
         } else {
@@ -213,7 +212,7 @@ function TicTacToe() {
         }
     };
 
-    this.switch_turn = function() {
+    this.switchTurn = function() {
         this.turn = !this.turn;
     };
 
@@ -221,11 +220,11 @@ function TicTacToe() {
         return t ? 1 : 0;
     };
 
-    this.is_game_over = function(board) {
-        return this.score_state(board) != 0;
+    this.isGameOver = function(board) {
+        return this.scoreState(board) != 0;
     };
 
-    this.has_room = function(board) {
+    this.hasRoom = function(board) {
         for(var i = 0; i < 3; i++) {
             for(var j = 0; j < 3; j++) {
                 if(board[i][j] == null) {
@@ -236,23 +235,23 @@ function TicTacToe() {
         return false;
     };
 
-    this.check_game_over = function() {
-        this.game_over = this.is_game_over(this.board);
-        if(this.game_over) {
-            this.set_game_over_span();
+    this.checkGameOver = function() {
+        this.gameOver = this.isGameOver(this.board);
+        if(this.gameOver) {
+            this.setGameOverSpan();
         } else {
-            if(!this.has_room(this.board)) { //Tie
-                this.game_over = true;
-                this.set_tie_span();
+            if(!this.hasRoom(this.board)) { //Tie
+                this.gameOver = true;
+                this.setTieSpan();
             }
         }
     };
 
-    this.set_game_over_span = function() {
+    this.setGameOverSpan = function() {
         this.$turn.text('Game Over!');
     };
 
-    this.set_tie_span = function() {
+    this.setTieSpan = function() {
         this.$turn.text("It's a Tie!");
     };
 
